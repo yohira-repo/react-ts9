e19dfa8d568d1618e90cf5633e649bcf8ac9bafd
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-09-03 12:34:42 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** basic type */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 0;
+
+// string
+let str: string = "a";
+
+// Array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tapple
+let tapple: [number, string] = [0, "A"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = () => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undef1: undefined = undefined;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 1, name: "AAAA" };
